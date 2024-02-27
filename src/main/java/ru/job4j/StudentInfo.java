a9a99a4580cb1deb1f38dbf20f2b4971c658e300
vadim <vagapov.v@mail.ru>
2024-02-27 21:22:26 +0500

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vadim Vagapov");
+        System.out.println("27-02-2024");
+    }
+}
