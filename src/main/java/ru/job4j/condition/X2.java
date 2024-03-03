32ed130f922c94b95562979e49c2c8f91f90f256
vadim <vagapov.v@mail.ru>
2024-03-03 20:24:36 +0500

    1.2. Входные данные в тесте [#297750 #484351] исправлено

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -7,10 +7,10 @@ public class X2 {
     }
 
     public static void main(String[] args) {
-        int a = 1;
-        int b = 1;
-        int c = 1;
-        int x = 0;
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
         int result = X2.calc(a, b, c, x);
         System.out.println(result);
     }
