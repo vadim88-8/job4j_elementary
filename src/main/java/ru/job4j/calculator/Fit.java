cbb744ca5eedab0af1dcacd4c63cd73d454900ae
vadim <vagapov.v@mail.ru>
2024-03-03 12:40:01 +0500

    2. Тесты для задачи "идеальный вес" [#159458]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double result = -1;
+        return result;
+    }
+
+    public static double womanWeight(short height) {
+        double result = -1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 187 is " + man);
+    }
+}
