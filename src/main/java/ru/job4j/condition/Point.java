e285a44c11773c7d94758522b40f3059f5f362db
vadim <vagapov.v@mail.ru>
2024-03-03 21:10:34 +0500

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,19 @@
 package ru.job4j.condition;
 
 public class Point {
+
     public static double distance(int x1, int y1, int x2, int y2) {
-        double result = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        return result;
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
-        double result2 = Point.distance(1, 5, 2, 7);
-        System.out.println("result (1, 5) to (2, 7) " + result2);
-        double result3 = Point.distance(2, 1, 3, 5);
-        System.out.println("result (2, 1) to (3, 5) " + result3);
+        double result2 = Point.distance(-1, 1, -2, 1);
+        System.out.println("result (-1, 1) to (-2, 1) " + result2);
+        double result3 = Point.distance(2, 1, 4, 4);
+        System.out.println("result (2, 1) to (4, 4) " + result3);
+        double result4 = Point.distance(1, 1, 5, 5);
+        System.out.println("result (1, 1) to (5, 5) " + result4);
     }
 }
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenMinus11toMinus22then1dot41() {
+        double expected = 1.41;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 2;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when21to44then3dot60() {
+        double expected = 3.60;
+        int x1 = 2;
+        int y1 = 1;
+        int x2 = 4;
+        int y2 = 4;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when11to55then5dot65() {
+        double expected = 5.65;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 5;
+        int y2 = 5;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
