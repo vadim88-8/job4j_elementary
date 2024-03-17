60efd5e7b098c149e1c304dfc400e4a89851a02a
vadim <vagapov.v@mail.ru>
2024-03-17 21:03:25 +0500

    5.0.1. Сумма чисел. [#226416] исправлено

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -10,9 +10,9 @@ public class Counter {
     }
 
     public static void main(String[] args) {
-//        System.out.println(sum(0, 10));
-//        System.out.println(sum(3, 8));
-//        System.out.println(sum(1, 1));
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
         System.out.println(sum(-3, 5));
     }
 }
