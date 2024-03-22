2cef8007ddf03c42f56199d11a21fd92243646b6
vadim <vagapov.v@mail.ru>
2024-03-22 13:09:55 +0500

    5.7. Ипотека [#156315 #487321]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,10 +4,8 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         percent /=  100;
-        double ostatok = amount + (amount * percent) - salary;
         while (amount > 0) {
-            amount = ostatok + (ostatok * percent);
-            ostatok = amount - salary;
+            amount += (amount * percent) - salary;
             year++;
         }
         return year;
