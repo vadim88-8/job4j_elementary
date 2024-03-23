5c058c973be2b78298bf80b0dda596863e9b6721
vadim <vagapov.v@mail.ru>
2024-03-23 21:55:04 +0500

    6.0.1. Объявление массива. [#3613]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+
     public static void main(String[] args) {
         short[] ages = new short[100];
         String[] surnames = new String[100500];
