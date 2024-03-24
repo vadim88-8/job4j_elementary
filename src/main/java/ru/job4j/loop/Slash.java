8ff503e54eeb7810901b55e65400e8204e722d19
vadim <vagapov.v@mail.ru>
2024-03-24 09:50:03 +0500

    5.9. Крест в псевдографике.  [#177182 #487626]

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -4,7 +4,7 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = row + cell == row * 2;
+                boolean left = row == cell;
                 boolean right = row + cell == size - 1;
                 if (left) {
                     System.out.print("0");
